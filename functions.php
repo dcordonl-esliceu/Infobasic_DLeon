58cf8d30a4e143f2bd24814a22cefb5a6a682a84
daniel <dcordonl@esliceu.net>
2026-01-30 16:57:32 +0100

    c

--- functions.php
+++ functions.php
@@ -18,6 +18,6 @@ add_action('after_setup_theme','infobasic_config', 0);
 function infobasic_scripts(){
     wp_enqueue_script('bootstrap_js', get_theme_file_uri('inc/js/bootstrap.bundle.min.js'), array('jquery'), '4.5', true);
     wp_enqueue_style('bootstrap_css', get_theme_file_uri('inc/css/bootstrap.min.css'), array(), '4.5', 'all');
-    wp_enqueue_style('style_css', get_theme_file_uri('style.css'), array(), '1.0', 'all');
+    wp_enqueue_style('style_css', get_theme_file_uri('style.css'), array(), filemtime(get_theme_file_path('style.css')), 'all');
 }
 add_action('wp_enqueue_scripts', 'infobasic_scripts');
