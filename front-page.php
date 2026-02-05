d5fa000d7d08e3cc1ef4590bd87f9285d509f966
daniel <dcordonl@esliceu.net>
2026-02-05 17:44:00 +0100

    frontpage actualziado

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <?php get_header(); ?>
-<div class="container">
+<div class="jumbotron jumbo-tienda">
     <main class="site-main">
         <section class="hero">
             <h1><?php bloginfo( 'name' ); ?></h1>
@@ -10,4 +10,21 @@
         </section>
     </main>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Últimas Entradas</h2>
+            <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="<?=site_url("blog")?>" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>      
+        </div>
+    </div>
+
+
 <?php get_footer(); ?>
---
+++ page-mang.php
@@ -0,0 +1,11 @@
+<?php
+    get_header();
+?>
+
+    <div class="jumbotron jumbo-mang">
+        <h1>Compra</h1>
+    </div>
+
+<?php
+    get_footer();
+?>
